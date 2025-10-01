0f86d51ffcb9c6a86ed7ec46ae81249696ddbfe6
Aljawharah Alsaab <aljoharahalsaab@gmail.com>
2025-10-01 22:03:58 +0300

    pharmacy

--- trustdose-admin/src/App.tsx
+++ trustdose-admin/src/App.tsx
@@ -1,6 +1,25 @@
-import React from "react";
-import AdminDashboard from "./Admindashboard";
+import { Routes, Route, Link } from "react-router-dom";
+import Pharmacy from "./Pharmacy";
+import Admindashboard from "./Admindashboard";
+
+function Home() {
+  return <h2 style={{ padding: 16 }}>Welcome to TrustDose 🚀</h2>;
+}
 
 export default function App() {
-  return <AdminDashboard />;
+  return (
+    <div>
+      <nav style={{ padding: 10, background: "#eee" }}>
+       
+        <Link to="/pharmacy">Pharmacy</Link> |{" "}
+        <Link to="/Admindashboard">Admindashboard</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pharmacy" element={<Pharmacy />} />
+        <Route path="/Admindashboard" element={<Admindashboard />} />
+      </Routes>
+    </div>
+  );
 }
