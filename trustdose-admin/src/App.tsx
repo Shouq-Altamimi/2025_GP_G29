9fc813146d4ff5fc084ea9128d8ad33bb82b8068
Daad <daadalali032@gmail.com>
2025-10-02 00:28:43 +0300

    update

--- trustdose-admin/src/App.tsx
+++ trustdose-admin/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Pharmacy from "./Pharmacy";
 import Admindashboard from "./Admindashboard";
+import Patient from "./Patient"; 
 
 function Home() {
   return <h2 style={{ padding: 16 }}>Welcome to TrustDose 🚀</h2>;
@@ -12,13 +13,17 @@ export default function App() {
       <nav style={{ padding: 10, background: "#eee" }}>
        
         <Link to="/pharmacy">Pharmacy</Link> |{" "}
-        <Link to="/Admindashboard">Admindashboard</Link>
+        <Link to="/Admindashboard">Admindashboard</Link>|{" "}
+        <Link to="/patient">Patient</Link> 
+
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pharmacy" element={<Pharmacy />} />
         <Route path="/Admindashboard" element={<Admindashboard />} />
+        <Route path="/patient" element={<Patient />} />
+
       </Routes>
     </div>
   );
