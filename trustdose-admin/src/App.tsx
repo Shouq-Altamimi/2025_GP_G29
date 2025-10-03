c370426b8139705b4e95f27661afd502b4769bb1
Aljawharah Alsaab <aljoharahalsaab@gmail.com>
2025-10-03 21:50:04 +0300

    patient info

--- trustdose-admin/src/App.tsx
+++ trustdose-admin/src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Pharmacy from "./Pharmacy";
 import Admindashboard from "./Admindashboard";
-import Patient from "./Patient"; 
+import Patient from "./Patient";
+import Doctor from "./Doctor";   // 👈 الاستيراد الجديد
 
 function Home() {
   return <h2 style={{ padding: 16 }}>Welcome to TrustDose 🚀</h2>;
@@ -11,11 +12,10 @@ export default function App() {
   return (
     <div>
       <nav style={{ padding: 10, background: "#eee" }}>
-       
         <Link to="/pharmacy">Pharmacy</Link> |{" "}
-        <Link to="/Admindashboard">Admindashboard</Link>|{" "}
-        <Link to="/patient">Patient</Link> 
-
+        <Link to="/Admindashboard">Admindashboard</Link> |{" "}
+        <Link to="/patient">Patient</Link> |{" "}
+        <Link to="/doctor">Doctor</Link>   
       </nav>
 
       <Routes>
@@ -23,7 +23,7 @@ export default function App() {
         <Route path="/pharmacy" element={<Pharmacy />} />
         <Route path="/Admindashboard" element={<Admindashboard />} />
         <Route path="/patient" element={<Patient />} />
-
+        <Route path="/doctor" element={<Doctor />} />  
       </Routes>
     </div>
   );
---
+++ trustdose-admin/src/firebase.ts
@@ -0,0 +1,16 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2m8eKUYpYCS17AoipbsbrQrZkK1mwtNM",
+  authDomain: "trustdose-96656.firebaseapp.com",
+  projectId: "trustdose-96656",
+  storageBucket: "trustdose-96656.firebasestorage.app",
+  messagingSenderId: "985521835267",
+  appId: "1:985521835267:web:221137ec5d8ff78348ed36",
+  measurementId: "G-K0EWZKBDT2",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
